237040f2b672cbf07bafd7e4213ab476be399aad
DHRUV CHOUDHARY <dhruvchoudhary177@gmail.com>
2026-02-28 21:07:11 +0530

    Add script to calculate average of temperature data
    This program heklps to understand the concept of the average in the python.

---
+++ average.py
@@ -0,0 +1,6 @@
+# This is the starting of the Numpy
+
+import numpy as np
+temperatures=np.array([32,55,68,5666,6749,1777])
+average=np.mean(temperatures)
+print("The average of the given data is:",average)
