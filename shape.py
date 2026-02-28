874a2ccd5db58ecd222d9bc56be446a7c437d896
DHRUV CHOUDHARY <dhruvchoudhary177@gmail.com>
2026-02-28 21:08:50 +0530

    Implement 2D arrays and shape printing
    Add two-dimensional arrays and print their shapes. by use of the numpy.

---
+++ shape.py
@@ -0,0 +1,13 @@
+import numpy as np
+#this is the 2 dimensional array 
+arr_2 = np.array([[1,2,3],
+                  [4,5,6]])
+
+#this is the another 2 dimensional array us ed in my code 
+#my approach to solev  this probloem
+arr_3= np.shape(np.array([[7,8,9],
+                          [10,11,12]]))
+
+print(arr_2)
+print(arr_2.shape)
+print(arr_3)
