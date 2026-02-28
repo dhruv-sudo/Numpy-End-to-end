0d3690f5cf97e9e143ec7f6547276c111629e62c
DHRUV CHOUDHARY <dhruvchoudhary177@gmail.com>
2026-02-28 21:10:14 +0530

    Create size.py to show numpy array size
    Add a script to demonstrate numpy array size in the numpy.

---
+++ size.py
@@ -0,0 +1,12 @@
+#in this code i should be able to understand about size in numpy array
+import numpy as np
+
+arr_4=np.array([
+                [[1,2,3],
+                [4,5,6]],
+                
+                [[2,4,5],
+                 [0,9,7]]])
+
+
+print("The size of the given array:",arr_4.size)
