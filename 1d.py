20fd16087d6016cb39ba56fd0e0ec2948b659cf1
DHRUV CHOUDHARY <dhruvchoudhary177@gmail.com>
2026-02-28 21:03:05 +0530

    Add program to calculate average of numpy array
    This numpy program help-s to calculate the average of the 1d array

---
+++ 1d.py
@@ -0,0 +1,5 @@
+#This program tells about how can i write 1 dimensional array in numpy
+import numpy as np
+marks=np.array([77,89,66,90,98,97])
+average=np.mean(marks)
+print("The average of the give data is:",average)
